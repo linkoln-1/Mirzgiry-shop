ce884dafc3dd6282af5e63f35a86db0144b0e7aa
Lincode <Lincode@mail.ru>
2023-06-01 19:38:05 +0300

    redirect

--- client/src/components/tabs-sub-components/logout/Logout.tsx
+++ client/src/components/tabs-sub-components/logout/Logout.tsx
@@ -1,21 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react'
 
-import { useAppDispatch } from '../../../hooks/hook'
+import { useAppDispatch, useAppSelector } from '../../../hooks/hook'
 import s from '../../../style/smallComponents/logout.module.scss'
-import { logOut } from '../../../store/applicationSlice/authorizationSlice';
+import { logOut } from '../../../store/applicationSlice/authorizationSlice'
+import { useNavigate } from 'react-router-dom'
 
-
-export const LogOut: React.FC =() => {
-    const dispatch = useAppDispatch()
-    const handleLogOut =()=>{
-        dispatch(logOut())
+export const LogOut: React.FC = () => {
+  const dispatch = useAppDispatch()
+  const token = useAppSelector(state => state.authorizationSlice.token)
+  const navigate = useNavigate()
+  const handleLogOut = () => {
+    void dispatch(logOut())
+  }
+  useEffect(() => {
+    if (!token) {
+      navigate('/')
     }
-    return (
+  }, [token])
+  return (
         <div className={s.logout}>
-           <button 
+           <button
            onClick={handleLogOut}
-           className={s.button}>ВЫХОД</button> 
+           className={s.button}>ВЫХОД</button>
         </div>
-    );
-};
-
+  )
+}
