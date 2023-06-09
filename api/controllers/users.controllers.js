111549764c269ca3b6892d6de088d37d0ed1f58a
aybika <adamovaaybika@mail.ru>
2023-06-09 22:05:06 +0300

    changeproduct

--- api/controllers/users.controllers.js
+++ api/controllers/users.controllers.js
@@ -83,59 +83,5 @@ module.exports.userscontroller = {
       next(e);
     }
   },
-  //   registerUser: async function (req, res) {
-  //
-  //       try{
-  //          const { login, password  } = req.body;
-  //          const passwordToString = password.toString()
-  //          const hash = await bcrypt.hash(passwordToString , Number(process.env.BCRYPT_ROUNDS));
-  //       const user = await User.create({
-  //           login: login,
-  //           password: hash,
-  //       });
-  //       res.json(user);
-  //       }catch(error){
-  //         return res.status(400).json(
-  //           "Ошибка при регистрации, пользователь с таким логином уже существует"
-  //         )
 
-  //       }
-
-  //     },
-
-  //     login: async (req, res) => {
-  //       const { login, password } = req.body;
-  //       const candidate = await User.findOne({ login });
-  //       if (!candidate) {
-
-  //         return res.status(401).json({
-  //           error:"Неверный логин"});
-  //       }
-  //       const valid = await bcrypt.compare(password, candidate.password);
-  //       if (!valid) {
-  //         return res.status(401).json({
-  //           error:"Неверный пароль"});
-  //       }
-
-  //       const payload = {
-  //         id: candidate._id,
-  //         login: candidate.login,
-  //       };
-  //       const token = await jwt.sign(payload, process.env.SECRET_JWT_KEY, {
-  //         expiresIn: "24h",
-  //       });
-  //       res.json({
-  //         token: token,feat__
-  //       });
-  //       return res.json('вы авторизированы');
-  //     },
-
-  // deleteUserById: async function (req, res) {
-  //   try {
-  //     const user = await User.findByIdAndRemove(req.params.id);
-  //     res.json("Пользователь удален");
-  //   } catch (error) {
-  //     console.log(error.toString());
-  //   }
-  // },
 };
